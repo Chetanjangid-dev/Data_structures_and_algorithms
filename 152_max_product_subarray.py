6cf91559891f8da7980f1a8af58ca21981cdb727
Chetan jangid <jangidchetan.jpr@gmail.com>
2026-03-21 00:21:56 +0530

    Create 152_max_product_subarray.py

---
+++ 152_max_product_subarray.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        max_prod = nums[0]
+        min_prod = nums[0]
+        result = nums[0]
+
+        for i in range(1, len(nums)):
+            num = nums[i]
+
+            # If number is negative, swap max and min
+            if num < 0:
+                max_prod, min_prod = min_prod, max_prod
+
+            # Either take current number or extend previous subarray
+            max_prod = max(num, max_prod * num)
+            min_prod = min(num, min_prod * num)
+
+            result = max(result, max_prod)
+
+        return result
