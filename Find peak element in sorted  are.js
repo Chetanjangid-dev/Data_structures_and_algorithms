39e127a30eccec4b1a3aa697ff7e694368e778bd
Chetan jangid <jangidchetan.jpr@gmail.com>
2026-04-13 16:17:45 +0530

    Create Find peak element in sorted  are.js

---
+++ Find peak element in sorted  are.js
@@ -0,0 +1,14 @@
+var findPeakElement = function(nums) {
+  let low=0
+  let high=nums.length-1
+  while (low<high){
+   mid=Math.floor((low+high)/2)
+   if( nums[mid]<nums[mid+1]){
+          low=mid+1
+   }
+   else{
+          high=mid    
+   }
+  }
+  return low  
+};
